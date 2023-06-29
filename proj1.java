b1c6776b5a7d25a3ea2621394d8c945fcb696561
ederxcv1 <138148566+ederxcv1@users.noreply.github.com>
2023-06-29 15:19:48 -0300

    Add files via upload

---
+++ proj1.java
@@ -0,0 +1,13 @@
+
+
+public class proj1 {
+    public static void main(String[] args) {
+
+
+        //Escreva um programa que imprima a mensagem "Meu nome é [seu nome]".
+        System.out.println("meu nome é:Eder ");
+
+
+        }
+    }
+
